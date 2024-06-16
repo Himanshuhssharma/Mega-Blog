f12ca0ea2eb01f18253fa74a85484b5a1c6fb5c3
Himanshu Sharma <himanshuhssharma9@gmail.com>
2024-06-17 01:27:41 +0530

    16 june

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useState, useEffect } from 'react';
+import {Header, Footer} from "./components/imports"
 import './App.css';
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice"
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) dispatch(login({userData}))
+      else dispatch(logout())
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return loading ? (null) : (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
