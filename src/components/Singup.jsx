4ca1d70476dad0e61ba692f1b4b7cd2b0c3a247c
Himanshu <himanshus@newgenit.com>
2024-06-18 18:14:15 +0530

    Commited after signup page

--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from "react";
+import authService from "../appwrite/auth";
 import { Link, useNavigate } from "react-router-dom";
-import { login as authLogin } from "../store/authSlice";
+import { login } from "../store/authSlice";
 import { Button, Input, Logo } from "./imports";
 import { useDispatch } from "react-redux";
-import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
 
-const Login = () => {
+const Singup = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [register, handleSubmit] = useForm(); // here handlesubmit is an event, so dont take it as a normal variable name
   const [err, setErr] = useState("");
+  const dispatch = useDispatch();
+  const [register, handleSubmit] = useForm();
 
-  const login = async (data) => {
+  const create = async (data) => {
     setErr("");
     try {
-      const session = await authService.login(data);
-      if (session) {
+      const filledData = await authService.createAccount(data);
+      if (filledData) {
         const userData = await authService.getCurrentUser();
-        if (userData) {
-          dispatch(authLogin(userData));
-          navigate("/");
-        }
+        if (userData) dispatch(login(userData));
+        navigate("/");
       }
     } catch (error) {
       setErr(error.message);
@@ -29,7 +27,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center">
       <div className="mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10">
         <div className="mb-2 flex justify-center">
           <span className="inline-block w-full max-w-[100px]">
@@ -37,26 +35,32 @@ const Login = () => {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign up to create account
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
+          Already have an account?&nbsp;
           <Link
-            to="/signup"
+            to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign Up
+            Sign In
           </Link>
         </p>
         {err && <p className="text-red-600 mt-8 text-center">{err}</p>}
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+        <form onSubmit={handleSubmit(create)}>
           <div className="space-y-5">
+            <Input
+              label="Full Name:"
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
             <Input
               label="email"
               placeholder="Enter your email:"
               type="email"
               {...register("email", {
-                //always spread this register, so that its value will not overwrite after using it again in other field
                 required: true,
               })}
             />
@@ -69,7 +73,7 @@ const Login = () => {
               })}
             />
             <Button type="submit" className="w-full">
-              Sign in
+              Create Account
             </Button>
           </div>
         </form>
@@ -78,4 +82,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Singup;
--- src/components/imports.js
+++ src/components/imports.js
@@ -3,11 +3,23 @@ import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
+import Input from "./Input"
+import PostCard from "./PostCard"
+import Button from "./Button"
+import Login from "./Login";
+import Select from "./Select"
+import Singup from "./Singup";
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    PostCard,
+    Button,
+    Login,
+    Select,
+    Singup
 }
