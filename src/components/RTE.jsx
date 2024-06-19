a989987b132c975ea2b841cd0176c2bc1d86a873
Himanshu <himanshus@newgenit.com>
2024-06-19 18:22:32 +0530

    19 jun

---
+++ src/components/RTE.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+const RTE = ({ name, control, label, defaultValue = "" }) => {
+  return (
+    <div className="w-full">
+      {label && <label className="text-sm text-gray-600">{label}</label>}
+    </div>
+  );
+};
+
+export default RTE;
